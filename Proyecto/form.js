5577961001eb2f147bc15acbfc696b5dfbe325d8
Agustin Miguez <143726897+Agust-Miguez@users.noreply.github.com>
2024-05-20 09:58:32 -0300

    add form.js
    error en un evento. no puedo solucionarlo

---
+++ Proyecto/form.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById('formulario');
+    const mensajeError = document.getElementById('mensajeError');
+
+    formulario.addEventListener('submit', function (evento) {
+        evento.preventDefault(); 
+
+        let nombre = document.getElementById('nombre').value;
+        let apellido = document.getElementById('apellido').value;
+        let fecha = document.getElementById('categoria').checked;
+        let email = document.getElementById('terminos').value;
+        let telefono = document.getElementById('tel').value;
+        let consulta = document.getElementById('consulta').value;
+        let novedades =document.getElementById("novedades").checked;
+
+        // Validación de los campos
+        if (nombre.trim() === '') {
+            mensajeError.innerText = 'El campo nombre no puede estar vacío.';
+            return;
+        }
+
+        if (apellido.trim() === '') {
+            mensajeError.innerText = 'El campo apellido no puede estar vacío.';
+            return;
+        }
+
+        if (email.trim() === '') {
+            mensajeError.innerText = 'El campo email no puede estar vacío.';
+            return;
+        }
+
+
+        if (!validarEmail(email)) {
+            mensajeError.innerText = 'El formato del email no es válido.';
+            return;
+        }
+
+        if (fecha === '') {
+            mensajeError.innerText = 'Debes seleccionar una fecha de nacimiento.';
+            return;
+        }
+
+        if (telefono.trim() === '') {
+            mensajeError.innerText = 'El campo telfono no puede estar vacío.';
+            return;
+        }
+
+        if (consulta.trim() === '') {
+            mensajeError.innerText = 'El campo consulta no puede estar vacío.';
+            return;
+        }
+
+        if (!novedades) {
+            mensajeError.innerText = 'Debes indicar tu preferencia.';
+            return;
+        
+       
+        mensajeError.innerText = '';
+        alert('Formulario enviado con éxito!');
+        formulario.submit(); 
+    }});
+
+    function validarEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+});
